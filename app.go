4d101c8f51b011ef67498ce3353ecc83b704de4f
Hugo Torres <hugo_torres@ultimatesoftware.com>
2018-07-16 09:02:48 -0400

    Refactor: use nested templates

---
+++ app.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+
+	"html/template"
+
+	"github.com/ghodss/yaml"
+)
+
+// App is the top level definition of the application being documented.
+type App struct {
+	Name      string `json:"app_name"`
+	Resources []Resource
+}
+
+// Page is an individual page that's generated.
+type Page struct {
+	AppName   string
+	Resources []Resource // need this here to generate all the links
+	Resource  Resource
+}
+
+// Resource is a logical grouping of endpoints.
+type Resource struct {
+	Name     string
+	Routes   []Route
+	Template *template.Template
+}
+
+// Route is an individual endpoint that can be interacted with.
+type Route struct {
+	Method      string
+	Path        string
+	Description string
+	Headers     map[string]string
+	Parameters  map[string]interface{}
+	Responses   []Response
+}
+
+// Response is one of the possible results from interacting with a Route.
+type Response struct {
+	Status      int
+	Description string
+	Headers     map[string]string
+	// Body is the YAML definition of what the response body can contain.
+	Body     map[string]interface{}
+	BodyJSON string `yaml:"-"`
+}
+
+// LoadApp reads "docky.yaml" in the current working directory and creates an
+// app definition from it.
+func LoadApp() (App, error) {
+	var cfg App
+	f, err := os.Open("docky.yaml")
+	if err != nil {
+		return App{}, err
+	}
+
+	buf, err := ioutil.ReadAll(f)
+	if err != nil {
+		return App{}, err
+	}
+
+	err = yaml.Unmarshal(buf, &cfg)
+	if err != nil {
+		return App{}, err
+	}
+
+	for _, resource := range cfg.Resources {
+		for _, route := range resource.Routes {
+			for i := range route.Responses {
+				resp := route.Responses[i]
+				schema, err := json.MarshalIndent(resp.Body, "", "  ")
+				if err != nil {
+					return App{}, err
+				}
+
+				resp.BodyJSON = string(schema)
+
+				route.Responses[i] = resp
+			}
+		}
+	}
+
+	return cfg, nil
+}
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"os"
+)
+
+const outFlags = os.O_WRONLY | os.O_CREATE | os.O_TRUNC
+const outMode = 0644
+
+var templates = []string{
+	"./templates/root.tpl.html",
+	"./templates/route.tpl.html",
+}
+
+func main() {
+	app, err := LoadApp()
+	if err != nil {
+		panic(err)
+	}
+
+	t, err := template.ParseFiles(templates...)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, r := range app.Resources {
+		filename := fmt.Sprintf("%v.html", r.Name)
+		out, err := os.OpenFile(filename, outFlags, outMode)
+		if err != nil {
+			panic(err)
+		}
+
+		pg := Page{AppName: app.Name, Resources: app.Resources, Resource: r}
+
+		err = t.Execute(out, pg)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
