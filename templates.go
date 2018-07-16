d79e99321a4f36c9ac1065919a94148c1e5cbd31
Hugo Torres <hugo_torres@ultimatesoftware.com>
2018-07-16 17:37:54 -0400

    Refactor root template to be more generic
    This paves the way for different content layouts, which will be needed
    for things like an index page.

--- templates.go
+++ templates.go
@@ -40,7 +40,7 @@ const root = `<!DOCTYPE html>
         </div>
     </nav>
     
-    {{ template "resource" .Resource }}
+    {{ template "body" .Resource }}
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
@@ -64,7 +64,7 @@ const root = `<!DOCTYPE html>
 </body>
 </html>`
 
-const route = `{{ define "resource" }}
+const route = `{{ define "body" }}
 <div class="base container">
     {{range $index, $element := .Routes}}
     <div class="row">
